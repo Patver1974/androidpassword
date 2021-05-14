87b363e5d902babbcc5323672f4483c1885e11ef
pve <patver1974@gmail.com>
2021-05-14 10:29:37 +0200

    toast

--- app/src/main/java/com/example/usernameformulaire/MainActivity.java
+++ app/src/main/java/com/example/usernameformulaire/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.usernameformulaire;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnClick = findViewById(R.id.button2);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast=Toast. makeText(getApplicationContext(),"J'ai fait  un click",Toast. LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+
+
     }
 }
